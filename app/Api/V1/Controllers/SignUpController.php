8ff58cddfa23457fbf97694afc517fe00f509142
Alexander <Viking0607@mail.ru>
2017-02-09 16:40:56 +0500

    Save fitness 09022017 16:40

--- app/Api/V1/Controllers/SignUpController.php
+++ app/Api/V1/Controllers/SignUpController.php
@@ -6,30 +6,67 @@ use Config;
 use App\User;
 use Tymon\JWTAuth\JWTAuth;
 use App\Http\Controllers\Controller;
-use App\Api\V1\Requests\LoginRequest;
+use App\Api\V1\Requests\SignUpRequest;
+use Dingo\Api\Routing\Helpers;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SignUpController extends Controller
 {
-    public function signUp(LoginRequest $request, JWTAuth $JWTAuth)
+    use Helpers;
+
+    public function signUp(SignUpRequest $request, JWTAuth $JWTAuth)
     {
-        $user = new User($request->all());
-        
 
-        if(!$user->save()) {
-            throw new HttpException(500);
+        $email = $request->email;
+        $pass  = $this->passGenerate();
+        $api_token = password_hash(uniqid(), PASSWORD_BCRYPT);
+
+        $user = new User();
+
+        if (User::where('email', $email)->count() > 0) {
+          return $this->response->noContent()->setStatusCode(500);
+        }
+        else {
+          $user->email = $email;
+          $user->password = $pass;
+          $user->activated_token = $api_token;
+          $user->save();
+
+          $data = array(
+              'email' => $email,
+              'password' => $pass,
+              'api_token' => $api_token
+          );
+
+          Mail::send('emails.registration', $data, function ($message) {
+              $message->from('ugrafitness@gmail.com', 'Ugra-fit');
+
+              $message->to(''.$email.'')->subject('Регистрация нового пользователя');
+          });
+          if(!Config::get('boilerplate.sign_up.release_token')) {
+              return response()->json([
+                  'status' => 'ok',
+              ], 201);
+          }
+
+          $token = $JWTAuth->fromUser($user);
+          return response()->json([
+              'status' => 'ok',
+              'token' => $token
+          ], 201);
         }
 
-        if(!Config::get('boilerplate.sign_up.release_token')) {
-            return response()->json([
-                'status' => 'ok'
-            ], 201);
-        }
-
-        $token = $JWTAuth->fromUser($user);
-        return response()->json([
-            'status' => 'ok',
-            'token' => $token
-        ], 201);
     }
+
+
+    private function passGenerate(){
+      $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+      $pass = array(); //remember to declare $pass as an array
+      $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+      for ($i = 0; $i < 8; $i++) {
+          $n = rand(0, $alphaLength);
+          $pass[] = $alphabet[$n];
+      }
+      return implode($pass); //turn the array into a string
+  }
 }
--- config/boilerplate.php
+++ config/boilerplate.php
@@ -5,9 +5,7 @@ return [
     'sign_up' => [
         'release_token' => env('SIGN_UP_RELEASE_TOKEN'),
         'validation_rules' => [
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required'
+            'email' => 'required|email'
         ]
     ],
 
