b66d2cea9397958cd710fd084125294545b79423
Vladislav <vladonxp@mail.ru>
2017-02-14 10:39:34 +0500

    production version

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var uglify = require('gulp-uglify');
 var buffer = require('vinyl-buffer');
 var concat = require('gulp-concat');
 
+process.env.NODE_ENV = 'production';
+
 gulp.task('build', function () {
     return browserify({entries: './app_js/index.jsx', extensions: ['.jsx'], debug: false})
         .transform('babelify', {presets: ['es2017', 'es2016', 'es2015', 'stage-0', 'stage-1', 'stage-2', 'react']})
