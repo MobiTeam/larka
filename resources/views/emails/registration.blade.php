54c705eb074ef0d9dec14842e0957a24619a16fc
Alexander Yakimchuk <Viking0607@mail.ru>
2017-02-14 13:11:08 +0500

    Edit mail 14022017T13:11

--- resources/views/emails/registration.blade.php
+++ resources/views/emails/registration.blade.php
@@ -1,42 +1,49 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+			<table style="color: #434343; max-width: 750px; font-family: Arial, Helvetica, sans-serif; border: 2px solid #fc671a; border-width: 5px 0px;">
+				<tr style="min-height: 100px;">
+					<td style="width: 50%">
+						<div class="div" style="padding-left: 1%;">
+							<h2 style="color: #f66514; display:inline-block; font-size: 28px;">В-Форме.рф</h2>
+						</div>
+					</td>
+					<td style="width: 50%; font-size: 14px; line-height: 1.3em; text-align: right; font-weight: bold; font-style: italic;">
+						<span>г. Ханты-Мансийск</span><br>
+						<span>ул. Студенческая 31</span><br>
+						89224418312 - Ирина
+					</td>
+				</tr>
+				<tr style="height: 25px;"></tr>
+				<tr>
+					<td colspan="2" style="font-size: 20px; text-align: center;">
+						<b>Здравствуйте</b>
+					</td>
+				</tr>
+				<tr style="height: 25px;"></tr>
+				<tr style="line-height: 1.3em">
+					<td colspan="2">
+						<p style="text-indent: 10px;">Поздравляем Вас с регистрацией на портале<a href="http://в-форме.рф/">В-Форме.рф</a></p>
+						<p style="text-indent: 10px;">Для подтверждения электронного адреса, указанного при регистрации, перейдите по данной <a href="http://bambarm2.beget.tech/auth/activated?token={!! $api_token !!}">ссылке</a></p>
+						<div>
+							<p style="text-indent: 10px;">Для входа на сайт используйте следующие учетные данные:</p>
+							<div style="background: #e8e8e8; padding: 10px; text-align: center;">
+								<p><b>Логин (email):</b> {!! $email !!}</p>
+								<p><b>Пароль:</b> {!! $password !!}</p>
+							</div>
+						</div>
 
-<table style="color: #434343; max-width: 750px; font-family: Arial, Helvetica, sans-serif; border: 2px solid #fc671a; border-width: 5px 0px;">
-	<tr style="min-height: 100px;">
-		<td style="width: 50%">
-			<div class="div" style="padding-left: 1%;">
-				<h2 style="color: #f66514; display:inline-block; font-size: 28px;">В-Форме.рф</h2>
-			</div>
-		</td>
-		<td style="width: 50%; font-size: 14px; line-height: 1.3em; text-align: right; font-weight: bold; font-style: italic;">
-			<span>г. Ханты-Мансийск</span><br>
-			<span>ул. Студенческая 31</span><br>
-			89224418312 - Ирина
-		</td>
-	</tr>
-	<tr style="height: 25px;"></tr>
-	<tr>
-		<td colspan="2" style="font-size: 20px; text-align: center;">
-			<b>Здравствуйте</b>
-		</td>
-	</tr>
-	<tr style="height: 25px;"></tr>
-	<tr style="line-height: 1.3em">
-		<td colspan="2">
-			<p style="text-indent: 10px;">Поздравляем Вас с регистрацией на портале<a href="В-Форме.рф">В-Форме.рф</a></p>
-			<p style="text-indent: 10px;">Для подтверждения электронного адреса, указанного при регистрации, перейдите по данной <a href="http://bambarm2.beget.tech/auth/activated?token={!! $api_token !!}">ссылке</a></p>
-			<div>
-				<p style="text-indent: 10px;">Для входа на сайт используйте следующие учетные данные:</p>
-				<div style="background: #e8e8e8; padding: 10px; text-align: center;">
-					<p><b>Логин (email):</b> {!! $email !!}</p>
-					<p><b>Пароль:</b> {!! $password !!}</p>
-				</div>
-			</div>
-
-		</td>
-	</tr>
-	<tr style="height: 25px;"></tr>
-	<tr>
-		<td colspan="2" style="font-style: italic; text-align: center;">Данное письмо было сгенерировано автоматически и отвечать на него не нужно.</td>
-	</tr>
-	<tr style="height: 25px;"></tr>
-	<tr style="height: 25px;"><td colspan="2" style="color: white; background: #fc671a; padding: 10px; text-align: center;">В-Форме.рф - <b>{!! $year !!}</b></td></tr>
-</table>
+					</td>
+				</tr>
+				<tr style="height: 25px;"></tr>
+				<tr>
+					<td colspan="2" style="font-style: italic; text-align: center;">Данное письмо было сгенерировано автоматически и отвечать на него не нужно.</td>
+				</tr>
+				<tr style="height: 25px;"></tr>
+				<tr style="height: 25px;"><td colspan="2" style="color: white; background: #fc671a; padding: 10px; text-align: center;">В-Форме.рф - <b>{!! $year !!}</b></td></tr>
+			</table>
+</body>
+</html>
