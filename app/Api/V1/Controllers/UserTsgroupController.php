3bb10521eed4266de568be9f95d77156e42ad735
Alexander <Viking0607@mail.ru>
2017-04-19 09:58:43 +0500

    Add check that user relation with this user group

--- app/Api/V1/Controllers/UserTsgroupController.php
+++ app/Api/V1/Controllers/UserTsgroupController.php
@@ -40,7 +40,7 @@ class UserTsgroupController extends Controller
         return $userGroup;
     }
 
-    // Привязка пользователя к тренировочной сезонной группе полностью
+    // Привязка пользователя к тренировочной сезонной группе c с полной оплатой
     public function create_relation(Request $request)
     {
         // Получаем пользователя и его баланс
@@ -53,40 +53,49 @@ class UserTsgroupController extends Controller
         $Group = info_group::find($group_id);
         // Стоимость сезона
         $costGroup = $Group->price;
-        // Проверяем хватает ли пользователю средств для записи на сезон
-        if ($userBalance >= $costGroup) {
-            // Проверяем имеются ли свободные места
-            $emptyCapacity = ($Group->capacity) - (info_group::find($group_id)->users()->count());
-            if ($emptyCapacity > 0) {
-                // Заносим данные по оплате в логи
-                $log = new Log_payments();
-                $log->user_id = $user->id;
-                // За какую группу оплата
-                $log->payments_id = $group_id;
-                $log->amount = $costGroup;
-                $log->type = 2;
-                $log->isApproved = 1;
-                $log->save();
+        // Проверяем, состоит ли пользователь уже в этой группе
+        $checkUserGroup = User_tsgroup::where([
+                                             ['user_id',$user->id],
+                                             ['info_group_id',$group_id]])->get()->count();
+        if(!($checkUserGroup > 0)) {
+            // Проверяем хватает ли пользователю средств для записи на сезон
+            if ($userBalance >= $costGroup) {
+                // Проверяем имеются ли свободные места
+                $emptyCapacity = ($Group->capacity) - (info_group::find($group_id)->users()->count());
+                if ($emptyCapacity > 0) {
+                    // Заносим данные по оплате в логи
+                    $log = new Log_payments();
+                    $log->user_id = $user->id;
+                    // За какую группу оплата
+                    $log->payments_id = $group_id;
+                    $log->amount = $costGroup;
+                    $log->type = 2;
+                    $log->isApproved = 1;
+                    $log->save();
 
-                // Вычитаем сумму вступления в группу из баланса пользователя
-                $user->balance -=$costGroup;
-                $user->save();
+                    // Вычитаем сумму вступления в группу из баланса пользователя
+                    $user->balance -=$costGroup;
+                    $user->save();
 
-                // Привязываем пользователя к определенной группе
-                $userTsgroup = new User_tsgroup();
-                $userTsgroup->user_id = $user->id;
-                $userTsgroup->info_group_id = $group_id;
-                $userTsgroup->leftPayd = 0;
-                $userTsgroup->save();
+                    // Привязываем пользователя к определенной группе
+                    $userTsgroup = new User_tsgroup();
+                    $userTsgroup->user_id = $user->id;
+                    $userTsgroup->info_group_id = $group_id;
+                    $userTsgroup->leftPayd = 0;
+                    $userTsgroup->save();
 
-                return response()->json(['message' => 'Запись проведена успешно!'], 200);
+                    return response()->json(['message' => 'Запись проведена успешно!'], 200);
+                }
+                else {
+                    return response()->json(['message' => 'Все места в группе уже заняты!'], 403);
+                }
             }
             else {
-                return response()->json(['message' => 'Все места в группе уже заняты!'], 403);
+                return response()->json(['message' => 'Недостаточно средств для записи в группу!'], 403);
             }
         }
         else {
-            return response()->json(['message' => 'Недостаточно средств для записи в группу!'], 403);
+            return response()->json(['message' => 'Вы уже состоите в этой группе!'], 403);
         }
     }
 
@@ -103,41 +112,51 @@ class UserTsgroupController extends Controller
         $Group = info_group::find($group_id);
         // Стоимость бронирования сезона
         $costBookingGroup = $Group->booking_price;
-        // Проверяем хватает ли пользователю средств для бронирования сезона
-        if ($userBalance >= $costBookingGroup) {
-            // Проверяем имеются ли свободные места
-            $emptyCapacity = ($Group->capacity) - (info_group::find($group_id)->users()->count());
-            if ($emptyCapacity > 0) {
-                // Заносим данные по оплате в логи
-                $log = new Log_payments();
-                $log->user_id = $user->id;
-                // За какую группу оплата
-                $log->payments_id = $group_id;
-                $log->amount = $costBookingGroup;
-                $log->type = 2;
-                $log->isApproved = 1;
-                $log->save();
 
-                // Вычитаем сумму бронирования группы из баланса пользователя
-                $user->balance -=$costBookingGroup;
-                $user->save();
+        // Проверяем, состоит ли пользователь уже в этой группе
+        $checkUserGroup = User_tsgroup::where([
+                                             ['user_id',$user->id],
+                                             ['info_group_id',$group_id]])->get()->count();
+        if(!($checkUserGroup > 0)) {
+            // Проверяем хватает ли пользователю средств для бронирования сезона
+            if ($userBalance >= $costBookingGroup) {
+                // Проверяем имеются ли свободные места
+                $emptyCapacity = ($Group->capacity) - (info_group::find($group_id)->users()->count());
+                if ($emptyCapacity > 0) {
+                    // Заносим данные по оплате в логи
+                    $log = new Log_payments();
+                    $log->user_id = $user->id;
+                    // За какую группу оплата
+                    $log->payments_id = $group_id;
+                    $log->amount = $costBookingGroup;
+                    $log->type = 2;
+                    $log->isApproved = 1;
+                    $log->save();
 
-                // Привязываем пользователя к определенной группе
-                $userTsgroup = new User_tsgroup();
-                $userTsgroup->user_id = $user->id;
-                $userTsgroup->info_group_id = $group_id;
-                // Заносим информацию о том, сколько необходимо пользователю доплатить
-                $userTsgroup->leftPayd = ($Group->price) - $costBookingGroup;
-                $userTsgroup->save();
+                    // Вычитаем сумму бронирования группы из баланса пользователя
+                    $user->balance -=$costBookingGroup;
+                    $user->save();
 
-                return response()->json(['message' => 'Запись проведена успешно, со временем необходимо оплатить сезон полностью'], 200);
+                    // Привязываем пользователя к определенной группе
+                    $userTsgroup = new User_tsgroup();
+                    $userTsgroup->user_id = $user->id;
+                    $userTsgroup->info_group_id = $group_id;
+                    // Заносим информацию о том, сколько необходимо пользователю доплатить
+                    $userTsgroup->leftPayd = ($Group->price) - $costBookingGroup;
+                    $userTsgroup->save();
+
+                    return response()->json(['message' => 'Запись проведена успешно, со временем необходимо оплатить сезон полностью'], 200);
+                }
+                else {
+                    return response()->json(['message' => 'Все места в группе уже заняты!'], 403);
+                }
             }
             else {
-                return response()->json(['message' => 'Все места в группе уже заняты!'], 403);
+                return response()->json(['message' => 'Недостаточно средств для бронирования!'], 403);
             }
         }
-        else{
-            return response()->json(['message' => 'Недостаточно средств для бронирования!'], 403);
+        else {
+            return response()->json(['message' => 'Вы уже состоите в этой группе!'], 403);
         }
     }
 
