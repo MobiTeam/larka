f051800ccdd590d178ba261134c224509b15ba12
Alexander <Viking0607@mail.ru>
2017-05-28 16:54:04 +0500

    save fitness 28052017

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -8,6 +8,67 @@
 
     <h1 class="clear-sans text-center"><b>Новости</b></h1>
     <div class="container news">
+            <!-- Вторая новость -->
+        <div class="well news-single">
+            <div class="media">
+                <a class="pull-left" href="#">
+                <img class="media-object news-img" src="base/img/news/3.jpg">
+                </a>
+                <div class="media-body">
+                <h4 class="media-heading">ФИТНЕС ПРОЕКТ и СЕМЕЙНЫЙ ЛАГЕРЬ!!! </h4>
+                <p class="text-right">Татьянкина Ирина</p>
+                <p class="clear-sans about-description-text">Коктебель!<br> 
+                    Это район Феодосии, рядом горы, скалы, море! Это богатые историей, любимые места Клементьева, Волошина, Айвазовского! 
+                <p>           
+                <p class="clear-sans about-description-text">Зачем мы туда едем?<br>
+                    Отдыхать! Заниматься спортом! Творить и развиваться!
+                    Глубже понять свои взаимоотношения с семьей!
+                <p>  
+                <p class="clear-sans about-description-text">Основные направления нашей программы:<br>
+                    фитнес, арт-терапия, свободный театр, экспериментариум, живопись
+                <p>
+                <p class="clear-sans about-description-text">В этом году у нас очень крутые организаторы:<br>
+                    <ul>
+                        <li><a href="https://vk.com/irina_dyubko">Ирина Дюбко</a> - мама маленькой принцессы, фитнес тренер,
+                            руководитель креативного фитнес проекта <a href="https://www.instagram.com/irina_vforme/">VFORME</a></li>
+                        <li><a href="https://vk.com/olga_ivasyuk">Ольга Ивасюк</a> - мама двух девчонок, педагог-психолог, 
+                            руководитель центра развития и творчества "Наша Школа" 
+                        </li>
+                        <li><a href="https://vk.com/id2328046">Оксана Мухарашева</a> многодетная мама, опытный
+                            путешественник с детьми, 
+                            творческая <a href="instagram.com/oksa_muha">личность</a>
+                        </li>
+                    </ul>
+                </p>
+                <p class="clear-sans about-description-text">По финансам 
+                    ОРГ.ВЗНОС (с семьи): 
+                    <ul>
+                        <li>10 000 - при оплате до конца мая;</li>
+                        <li>14 000 - при оплате до конца июня; </li>
+                        <li>18 000 - при оплате до конца июля</li>
+                    </ul>
+                </p>
+                <p class="clear-sans about-description-text">Проживание<br>
+                    База отдыха находится у подножия потухшего вулкана Карадаг прямо на берегу МОРЯ!<br>
+                    Питание будет организовано в кафе неподалеку, где работает линия раздачи, и каждая семья может выбрать СВОИ ЛЮБИМЫЕ блюда.<br>    
+                    Минимальная СТОИМОСТЬ проживания и питания на семью 20 000р.!!! (двое взрослых и ребенок) 
+                </p>
+                <p class="clear-sans about-description-text">Контакты: 
+                    <ul>
+                        <li><a href="https://vk.com/id2328046">Оксана Мухарашева</a>: 8(904)872-07-65</li>
+                        <li><a href="https://vk.com/olga_ivasyuk">Ольга Ивасюк</a>: 8(904)886-82-22 <a href="https://vk.com/nashashkolahm">"Наша школа" развития и творчества</a> </li>
+                        <li><a href="https://vk.com/irina_dyubko">Ирина Дюбко</a>: 8(922)441-83-12</li>
+                    </ul>
+                </p>
+                <p class="clear-sans about-description-text"><b>Подробности в группе <a href="https://vk.com/go_family_camp">Вконтакте</a>:</b>
+                </p>        
+                <ul class="list-inline list-unstyled">
+                    <li><span><i class="glyphicon glyphicon-calendar"></i>Добавлено  28 мая 2017 </span></li>
+                </ul>
+             </div>
+          </div>
+        </div>
+
         <!-- Первая новость -->
         <div class="well news-single">
             <div class="media">
@@ -34,6 +95,5 @@
              </div>
           </div>
         </div>
-
     </div>
 @endsection
